9e4934ca813388c59142b2e4aab67375a76d4723
EunHyunsu <ehs208@naver.com>
2024-08-24 18:52:03 +0900

    어노테이션 기반으로 개선

--- src/main/java/com/example/springboot/web/IndexController.java
+++ src/main/java/com/example/springboot/web/IndexController.java
@@ -1,11 +1,10 @@
 package com.example.springboot.web;
 
+import com.example.springboot.config.auth.LoginUser;
 import com.example.springboot.config.auth.dto.SessionUser;
 import com.example.springboot.web.dto.PostsResponseDto;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +18,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
